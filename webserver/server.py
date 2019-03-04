1b5b5ac042ff8cdbdb1cf1866186bbb53e5c3c6a
twinklegupta <twinkle.gupta@yahoo.com>
2019-03-03 22:15:55 -0800

    paginf

--- webserver/server.py
+++ webserver/server.py
@@ -249,6 +249,7 @@ def get_producer_table():
 
 @app.route('/get_award_table')
 def get_award_table():
+    #TODO paging support
     cursor = g.conn.execute("SELECT A.name, A.year, A.category, M.name FROM award_given as A, motion_picture as M WHERE A.pic_id = M.pic_id")
     names = []
     for result in cursor:
